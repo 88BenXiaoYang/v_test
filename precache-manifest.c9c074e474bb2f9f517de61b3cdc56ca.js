bdfa05b767bac6781ce68b86c8aefb6471ecb042
by <993475269@qq.com>
2021-07-09 17:32:55 +0800

    new project

---
+++ precache-manifest.c9c074e474bb2f9f517de61b3cdc56ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0cbb5261d703eee5b82",
+    "url": "/css/app.356133e6.css"
+  },
+  {
+    "revision": "567062979dc243bcd83b378f9dc9dc5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0cbb5261d703eee5b82",
+    "url": "/js/app.75955f20.js"
+  },
+  {
+    "revision": "455a934d1d471df2d02f",
+    "url": "/js/chunk-vendors.0f99fb32.js"
+  },
+  {
+    "revision": "df5d7aae11d16ef426dcc6f4f7917f6c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
